d0ca6387cf992575227e7f33b62b11a06ff0503f
Flurry <41346632+Flurry2005@users.noreply.github.com>
2026-06-13 21:09:23 +0200

    Fix

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,27 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const url = `https://discordksfalertbot.onrender.com/`;
+const interval = 30000;
+
+//Reloader Function
+function reloadWebsite() {
+  fetch(url)
+    .then((response) => {
+      console.log(
+        `Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`,
+      );
+    })
+    .catch((error) => {
+      console.error(
+        `Error reloading at ${new Date().toISOString()}:`,
+        error.message,
+      );
+    });
+}
+
+setInterval(reloadWebsite, interval);
+
 type GuildData = {
   maps: GuildMap[];
 };
